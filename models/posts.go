cc4745e63c12333c699448776dce5694af896d71
Niall1985 <dcunhaniall@gmail.com>
2024-07-21 21:43:40 +0530

    modified the json database to append new posts to a new line

--- models/posts.go
+++ models/posts.go
@@ -1,102 +1,3 @@
-// package models
-
-// import (
-// 	"encoding/json"
-// 	"os"
-// 	"sync"
-// )
-
-// type Post struct {
-// 	ID       int    `json:"id"`
-// 	Title    string `json:"title"`
-// 	Username string `json:"username"`
-// 	Content  string `json:"content"`
-// }
-
-// var (
-// 	posts     []Post
-// 	idCounter = 1
-// 	mut       sync.Mutex
-// 	filePath  = "posts.json" // Path to the JSON file
-// )
-
-// func LoadPosts() error {
-// 	mut.Lock()
-// 	defer mut.Unlock()
-
-// 	file, err := os.Open(filePath)
-// 	if err != nil {
-// 		if os.IsNotExist(err) {
-// 			return nil // File does not exist; return an empty list
-// 		}
-// 		return err
-// 	}
-// 	defer file.Close()
-
-// 	decoder := json.NewDecoder(file)
-// 	if err := decoder.Decode(&posts); err != nil {
-// 		return err
-// 	}
-// 	if len(posts) > 0 {
-// 		idCounter = posts[len(posts)-1].ID + 1
-// 	}
-// 	return nil
-// }
-
-// func savePosts() error {
-// 	mut.Lock()
-// 	defer mut.Unlock()
-
-// 	file, err := os.Create(filePath)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer file.Close()
-
-// 	encoder := json.NewEncoder(file)
-// 	return encoder.Encode(posts)
-// }
-
-// func GetAllPosts() []Post {
-// 	return posts
-// }
-
-// func GetPostByID(id int) *Post {
-// 	for _, post := range posts {
-// 		if post.ID == id {
-// 			return &post
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func CreatePost(title, username, content string) {
-// 	post := Post{ID: idCounter, Title: title, Username: username, Content: content}
-// 	posts = append(posts, post)
-// 	idCounter++
-// 	savePosts()
-// }
-
-// func UpdatePost(updatedPost *Post) {
-// 	for i, post := range posts {
-// 		if post.ID == updatedPost.ID {
-// 			posts[i] = *updatedPost
-// 			savePosts()
-// 			break
-// 		}
-// 	}
-// }
-
-// func DeletePost(id int) {
-// 	for i, post := range posts {
-// 		if post.ID == id {
-// 			posts = append(posts[:i], posts[i+1:]...)
-// 			savePosts()
-// 			break
-// 		}
-// 	}
-// }
-
 package models
 
 import (
@@ -188,7 +89,7 @@ func UpdatePost(updatedPost *Post) {
 	for i, post := range posts {
 		if post.ID == updatedPost.ID {
 			posts[i] = *updatedPost
-			saveAllPosts() // Save all posts since updating a specific post requires rewriting the file
+			saveAllPosts()
 			break
 		}
 	}
@@ -198,7 +99,7 @@ func DeletePost(id int) {
 	for i, post := range posts {
 		if post.ID == id {
 			posts = append(posts[:i], posts[i+1:]...)
-			saveAllPosts() // Save all posts since deleting a specific post requires rewriting the file
+			saveAllPosts()
 			break
 		}
 	}
